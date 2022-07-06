91147446a5dedabfc6125be8b751140d4d8bb682
Raphael Vieira Rossi <raphael.rossi@g.globo>
2022-07-06 16:31:15 -0300

    Release 3.2.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ RUNTIME_REQUIREMENTS = [
 setup(
     long_description=readme,
     name="remotecv",
-    version="3.2.1",
+    version="3.2.2",
     description="remotecv is an OpenCV worker for facial and feature recognition",
     python_requires="==3.*,>=3.7.0",
     author="Bernardo Heynemann",
